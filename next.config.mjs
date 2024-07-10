711f01dcd6158fa2ba7fb6605848c8e24aa6c431
jhooper04 <jhooper04@users.noreply.github.com>
2024-07-10 12:47:27 -0500

    Enabled trailingSlash option

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     output: 'export',
     distDir: process.env.OUTPUT_DIR,
     images: { unoptimized: true },
+    trailingSlash: true,
 };
 
 if (process.env.NODE_ENV == 'development') {
